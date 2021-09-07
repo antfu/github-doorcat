e78784c6fc303dbbb537b80a196932f5ad52429e
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-07 14:05:51 +0800

    Initial commit

---
+++ vite.config.content.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import WindiCSS from 'vite-plugin-windicss'
+import { sharedConfig } from './vite.config'
+import { r, isDev } from './scripts/utils'
+import windiConfig from './windi.config'
+
+// bundling the content script using Vite
+export default defineConfig({
+  ...sharedConfig,
+  build: {
+    watch: isDev
+      ? {
+        include: [
+          r('src/contentScripts/**/*'),
+          r('src/components/**/*'),
+        ],
+      }
+      : undefined,
+    outDir: r('extension/dist/contentScripts'),
+    cssCodeSplit: false,
+    emptyOutDir: false,
+    sourcemap: isDev ? 'inline' : false,
+    lib: {
+      entry: r('src/contentScripts/index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.global.js',
+      },
+    },
+  },
+  plugins: [
+    ...sharedConfig.plugins!,
+
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS({
+      config: {
+        ...windiConfig,
+        // disable preflight to avoid css population
+        preflight: false,
+      },
+    }),
+  ],
+})
---
+++ vite.config.ts
@@ -0,0 +1,106 @@
+import { defineConfig, UserConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
+import WindiCSS from 'vite-plugin-windicss'
+import AutoImport from 'unplugin-auto-import/vite'
+import windiConfig from './windi.config'
+import { r, port, isDev } from './scripts/utils'
+
+export const sharedConfig: UserConfig = {
+  root: r('src'),
+  resolve: {
+    alias: {
+      '~/': `${r('src')}/`,
+    },
+  },
+  define: {
+    __DEV__: isDev,
+  },
+  plugins: [
+    Vue(),
+
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'webextension-polyfill': [
+            ['default', 'browser'],
+          ],
+        },
+      ],
+      dts: r('src/auto-imports.d.ts'),
+    }),
+
+    Components({
+      dirs: [r('src/components')],
+      // generate `components.d.ts` for ts support with Volar
+      dts: true,
+      resolvers: [
+        // auto import icons
+        // https://github.com/antfu/vite-plugin-icons
+        IconsResolver({
+          componentPrefix: '',
+        }),
+      ],
+    }),
+
+    // https://github.com/antfu/vite-plugin-icons
+    Icons(),
+
+    // rewrite assets to use relative path
+    {
+      name: 'assets-rewrite',
+      enforce: 'post',
+      apply: 'build',
+      transformIndexHtml(html) {
+        return html.replace(/"\/assets\//g, '"../assets/')
+      },
+    },
+  ],
+  optimizeDeps: {
+    include: [
+      'vue',
+      '@vueuse/core',
+    ],
+    exclude: [
+      'vue-demi',
+    ],
+  },
+}
+
+export default defineConfig(({ command }) => ({
+  ...sharedConfig,
+  base: command === 'serve' ? `http://localhost:${port}/` : undefined,
+  server: {
+    port,
+    hmr: {
+      host: 'localhost',
+    },
+  },
+  build: {
+    outDir: r('extension/dist'),
+    emptyOutDir: false,
+    sourcemap: isDev ? 'inline' : false,
+    // https://developer.chrome.com/docs/webstore/program_policies/#:~:text=Code%20Readability%20Requirements
+    terserOptions: {
+      mangle: false,
+    },
+    rollupOptions: {
+      input: {
+        background: r('src/background/index.html'),
+        options: r('src/options/index.html'),
+        popup: r('src/popup/index.html'),
+      },
+    },
+  },
+  plugins: [
+    ...sharedConfig.plugins!,
+
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS({
+      config: windiConfig,
+    }),
+  ],
+}))
